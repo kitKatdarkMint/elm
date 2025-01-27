180627fd0171f1e10785e1a0d36ac90aec6abf09
kitKatdarkMint <etincelle706@gmail.com>
2025-01-27 14:14:33 +0100

    Create parsing.elm

---
+++ parsing.elm
@@ -0,0 +1,2 @@
+{- module de parsing qui fournit une fonction read prenant en entrée une chaîne de caractères et retournant une structure de données que vous aurez définie pour représenter les programmes TcTurtle.
+Vous aurez besoin des fonctions succeed, token, int, spaces, lazy, run du package Parser -}
