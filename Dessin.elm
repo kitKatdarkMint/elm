f333a9c6c5cae07a201f43f65eceed55a8f1105e
kitKatdarkMint <etincelle706@gmail.com>
2025-01-28 14:55:36 +0100

    Update and rename dessin.elm to Dessin.elm

--- Dessin.elm
+++ Dessin.elm
@@ -1,4 +1,6 @@
 {-module de dessin qui fournit une fonction display traduisant votre stucture de données en un élément svg -}
-module dessin exposing (..)
+module Dessin exposing (..)
 
 import Svg exposing (..)
+
+
