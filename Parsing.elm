455252b4dbe0fcbb5565c25b7d2fb36f0a6ccf96
kitKatdarkMint <etincelle706@gmail.com>
2025-01-28 10:05:59 +0100

    Update Parsing.elm

--- Parsing.elm
+++ Parsing.elm
@@ -10,20 +10,37 @@ type Instruction
     | Left Int
     | Right Int
     | Repeat Int (List Instruction)
-block : Parser (List Instruction) --afin d'accéder de manière recursive liste dans liste
-block = 
+
+    
+block : Parser (List Instruction) --afin d'accéder de manière recursive bloc d'instructions dans un autre bloc d'instructions
+block = sequence --fonction de Parser qui recoit une liste d'instructions dans un ordre donné
+    {start = "["
+    , separator = ","
+    , end = "]"
+    , spaces = spaces --ignore les espaces 
+    , item = instruction}
+    
 instructionUser : Parser Instruction
 {-parseur oneOf qui permet d'essayer plusieurs parseurs dans l'ordre jusqu'à ce qu'un réussisse -}
 instructionUser = oneOf [succeed  Forward 
                                 |."Forward" --utilisation du .| pour ecarter resultat de la suite
+                                |. spaces --ignore les espaces
+                                |= int --recuperation de l'entier
                         ,succeed Left 
                                 |. "Left"
+                                |. spaces --ignore les espaces
+                                |= int --recuperation de l'entier
                         ,succeed Right 
                                 |. "Right"
+                                |. spaces --ignore les espaces
+                                |= int --recuperation de l'entier
                         ,succeed Repeat 
                                 |. "Repeat"
+                                |. spaces --ignore les espaces
+                                |= int --recuperation de l'entier
+                                |. spaces --si il y a à nouveau espace entre crochet et entier
                                 |= lazy (\_ -> block)
                         ]
 -- exécuter un parser sur une chaîne de caractères donnée
-runBlock :String -> Result (List DeadEnd) (List Instruction)
+runBlock :String -> Result (List DeadEnd) (List Instruction) --List DeadEnd correspond liste d'erreurs
 runBlock userString = run block userString
