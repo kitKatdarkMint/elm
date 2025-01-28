b40b76b3a5e86a59511ccf41db69c135697dd988
PH-Lucotte <paul-henri.lucotte@insa-lyon.fr>
2025-01-28 15:13:21 +0100

    ajout fonction calcEndpoint

--- Dessin.elm
+++ Dessin.elm
@@ -7,3 +7,18 @@ rotation pos instruction =
 		case instruction of
 	            Right changeAngle -> { pos | angle = pos.angle -changeAngle }
 	            Left changeAngle -> {pos | angle = pos.angle + changeAngle}
+
+calcEndpoint : Pos -> Float -> Pos
+calcEndpoint start distance =
+    let
+        -- Compute the endpoint
+        end =
+            { x = start.x + distance * cos start.angle
+            , y = start.y + distance * sin start.angle
+            , angle = start.angle
+            }
+        
+        -- Generate the line trace
+        trace = line start end
+    in
+    end 
