426f2c4dd0e5432533891ec55dd6aa0a63c3fd71
kitKatdarkMint <etincelle706@gmail.com>
2025-01-28 15:11:02 +0100

    Update Dessin.elm

--- Dessin.elm
+++ Dessin.elm
@@ -3,4 +3,7 @@ module Dessin exposing (..)
 
 import Svg exposing (..)
 
-
+rotation pos instruction = 
+		case instruction of
+            Right changeAngle -> { pos | angle = pos.angle -changeAngle }
+            Left changeAngle -> {pos | angle = pos.angle + changeAngle}
